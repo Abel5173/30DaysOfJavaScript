f1192b9e043fd5e17047bb908bc3ddca1a1fb3b4
Abel Zeleke <abelzeleke5173@gmail.com>
2022-08-21 19:17:25 +0300

    Finished Exercise 2

--- Day 5: Exercises/Exercise2.js
+++ Day 5: Exercises/Exercise2.js
@@ -4,5 +4,54 @@
 
 let text =
 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = text.split(' ')
 console.log(words)
 console.log(words.length)
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+shoppingCart.unshift('Meat')
+shoppingCart.push('Sugar')
+shoppingCart.splice(4,1)
+shoppingCart[3] = 'Green Tea'
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+
+if(countries.includes('Ethiopia')){
+    console.log('ETHIOPIA')
+} else{
+    countries.push('Ethiopia')
+}
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+
+  if(webTechs.includes('Saas')){
+    console.log('Sass is a CSS preprocess')
+} else{
+    webTechs.push('Saas')
+    console.log(webTechs)
+}
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd)
+console.log(fullStack)
